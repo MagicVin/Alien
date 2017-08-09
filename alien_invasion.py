8db85659b0dee20530052f90392819513c18ff8e
MagicVin <xinx.e.zhang@intel.com>
2017-08-09 17:00:06 +0800

    add alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import pygame
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode(1200,800)
+    pygame.display.set_caption("Alien Invasion")
+
+    #开始游戏的主循环
+    while True:
+
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            #让最近描绘的屏幕可见
+            pygame.dissplay.flip()
+
+run_game()
